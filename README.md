194a0f550593344d13e69b92488c1079710456b7
arnaudtalom <talombouopdaarnaud@gmail.com>
2021-07-22 19:52:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-yuqwce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-yuqwce)
---
+++ pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  time: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  timer: number;
+  constructor(public navCtrl: NavController) {}
+  startTimer(duration: number) {
+    this.timer = duration * 60;
+    setInterval(() => {
+      this.updateTimeValue();
+    }, 1000);
+  }
+
+  updateTimeValue() {
+    let minutes: any = this.timer / 60;
+    let seconds: any = this.timer % 60;
+    minutes = String('0' + Math.floor(minutes)).slice(-2);
+    seconds = String('0' + Math.floor(seconds)).slice(-2);
+    const text = minutes + ':' + seconds;
+    this.time.next(text);
+    --this.timer;
+    if (this.timer < 0) {
+      this.startTimer(5);
+    }
+  }
+}
